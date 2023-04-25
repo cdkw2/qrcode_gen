cf8ecdd8551cd4b425d6ef89122719b39700af9e
cdkw2 <67598073+cdkw2@users.noreply.github.com>
2023-04-25 22:22:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import qrcode
-from PIL import Image
 
 url = input("Enter the URL to generate QR code for: ")
 
-qr = qrcode.QRCode(version=1, box_size=10, border=5)
+qr = qrcode.QRCode(version=1, box_size=1, border=4)
 qr.add_data(url)
 qr.make(fit=True)
 
-img = qr.make_image(fill_color="black", back_color="white")
-img.show()
+matrix = qr.get_matrix()
+
+for row in matrix:
+    line = ""
+    for col in row:
+        if col:
+            line += "  "
+        else:
+            line += "██"
+    print(line)
